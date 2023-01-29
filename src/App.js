40d18c99c1c0c966dd0fe77b755b9cb4f9a4203b
Дмитрий Маклаков <dimonabinsk@yandex.ru>
2023-01-29 21:42:01 +0300

    создал приложение

--- src/App.js
+++ src/App.js
@@ -1,23 +1,104 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter,
+  Navigate,
+  NavLink,
+  Outlet,
+  Route,
+  Routes,
+  useParams,
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>User App</h1>
+      <BrowserRouter>
+        <NavLink to="/users">Users List Page</NavLink>
+        <Routes>
+          <Route path="" element={<MainPage />} />
+          <Route path="users" element={<UsersLayout />}>
+            <Route index element={<UsersListPage />} />
+            <Route path=":userId" element={<Outlet />}>
+              <Route path="profile" element={<UserProfilePage />} />
+              <Route path="edit" element={<EditUserPage />} />
+              <Route index element={<Navigate to="./profile" />} />
+              <Route path="*" element={<Navigate to="../profile" />} />
+            </Route>
+          </Route>
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+function MainPage() {
+  return (
+    <>
+      <h2>MainPage</h2>
+    </>
+  );
+}
+
+function UsersLayout() {
+  return (
+    <div>
+      <h2>Users Layout</h2>
+      <NavLink to="/">Main Page</NavLink>
+      <Outlet />
+    </div>
+  );
+}
+
+function UsersListPage() {
+  return (
+    <div>
+      <h3>User Page</h3>
+      <ul>
+        {new Array(5).fill("").map((_, i) => (
+          <li key={"user_list" + i}>
+            <NavLink to={i + 1 + "/profile"}>User {i + 1}</NavLink>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function UserProfilePage() {
+  const { userId } = useParams();
+  return (
+    <div>
+      <h2>User Page</h2>
+      <ul>
+        <li>
+          <NavLink to="/users">Users List page</NavLink>
+        </li>
+        <li>
+          <NavLink to={`/users/${userId}/edit`}>Edit this user</NavLink>
+        </li>
+      </ul>
+      <p>user ID: {userId}</p>
+    </div>
+  );
+}
+
+function EditUserPage() {
+  const { userId } = useParams();
+  return (
+    <div>
+      <h2> Edit User Page</h2>
+      <ul>
+        <li>
+          <NavLink to={"/users/" + userId}>User profile Page</NavLink>
+        </li>
+        <li>
+          <NavLink to={"/users/" + (+userId + 1)}> Another User</NavLink>
+        </li>
+        <li>
+          <NavLink to={"/users"}> Users List page</NavLink>
+        </li>
+      </ul>
     </div>
   );
 }
